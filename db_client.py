ae08847d1c1d68c79e4fa6f03374a074ac8ed19f
marky0470 <mark.soriano04@yahoo.com>
2026-04-06 11:21:48 +0800

    command handler and audio service refactors

--- db_client.py
+++ db_client.py
@@ -15,8 +15,14 @@ class DatabaseClient():
         result = self.collection.find_one({"user_id": user_id})
         return result["audio_data"] if result else None
 
-    def insert_audio(self, user_id, audio_data: bytes) -> None:
-        self.collection.insert_one({"user_id": user_id, "use_audio": True, "audio_data": audio_data})
+    def insert_audio(self, user_id: int, username: str, audio_data: bytes) -> None:
+        data = {
+            "user_id": user_id,
+            "username": username,
+            "use_audio": True,
+            "audio_data": audio_data
+        }
+        self.collection.insert_one(data)
 
     def update_audio(self, user_id, audio_data: bytes) -> None:
         self.collection.update_one({"user_id": user_id}, {"$set": {"use_audio": True, "audio_data": audio_data}}, upsert=True)
